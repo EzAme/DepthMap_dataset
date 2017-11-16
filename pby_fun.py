847f841c7c9220152e24c2326b26a63a050fd1bc
brothaman <rbrothers12@gmail.com>
2017-11-15 21:41:26 -0600

    tested generation of cubes will add generation of cones, cylinders, background, and rowdy STL file. (thanks LOU)

--- pby_fun.py
+++ pby_fun.py
@@ -5,16 +5,19 @@ def create_random_cube(
         objID,
         magnitude, 
         boundary=[(0,1),(0,1),(0,1)],
+        size = [0.01, 0.03]
         ):
     """
     this function has been created to generate randomly sized triangles between
     user-defined dimensions, reflectivity, color, and 
     """
-    magn = 5
-    loc = ( (0.5-rand.random())*magn, (0.5-rand.random())*magn, (0.5-rand.random())*magn)
+    loc = ( (0.5-rand.random())*magnitude, (0.5-rand.random())*magnitude, (0.5-rand.random())*magnitude)
 
     # create a cube object
-    bpy.ops.mesh.primitive_cube_add( location= loc)
+    bpy.ops.mesh.primitive_cube_add( 
+        location= loc,
+        radius=rand.uniform(size[0], size[1])
+        )
     bpy.ops.transform.rotate(
             value=rand.random()*3.14, 
             axis=(rand.random(),rand.random(), rand.random()),
@@ -22,14 +25,21 @@ def create_random_cube(
             mirror=False,
             proportional='DISABLED',
             proportional_edit_falloff='SMOOTH',
-            propoartional_size=1
+            proportional_size=1
             )
-    bpy.ops.material.new()
+    
 
-    # create a texture and color
-    mat1 = bpy.data.materials['Material'+objID]
-    mat1.diffuse_color = ( rand.random(), rand.random(), rand.random())
-    bpy.data.objects['Cube'+objID].data.materials.append(mat1)
+    # create and add a texture and color
+    bpy.ops.material.new()
+    if not len(objID):
+        mat = bpy.data.materials['Material']
+        mat.diffuse_color = ( rand.random(), rand.random(), rand.random())
+        bpy.data.objects['Cube'].data.materials.append(mat)
+    else:
+        mat = bpy.data.materials['Material.'+objID]
+        mat.diffuse_color = ( rand.random(), rand.random(), rand.random())
+        bpy.data.objects['Cube.'+objID].data.materials.append(mat)
+    
 
 
 def create_background():
@@ -48,18 +58,52 @@ def create_background():
                 False, False, False, False, False
                 )
             )
+    #bpy.ops.transform.rotate(value=rand.random()*3.14, 
     # create plane 2
     
 def clean_up_scene():
+    # clear scene of all stuff (from the depthmap_dataset) 
+    for material in bpy.data.materials:
+        material.user_clear();
+        bpy.data.materials.remove(material);
+
+    for texture in bpy.data.textures:
+        texture.user_clear();
+        bpy.data.textures.remove(texture);
+
+    # clear default nodes
+    bpy.context.scene.use_nodes = True
+    tree = bpy.context.scene.node_tree
+    links = tree.links
+    
+    for n in tree.nodes:
+        tree.nodes.remove(n)
+                
     #Remove objects from previsous scenes
     for item in bpy.data.objects:  
         if item.type == "MESH":  
             bpy.data.objects[item.name].select = True
             bpy.ops.object.delete()
 
-        for item in bpy.data.meshes:
-            bpy.data.meshes.remove(item)
+    for item in bpy.data.meshes:
+         bpy.data.meshes.remove(item)
+         
+    return tree, links
 
 def makeascene():
-    clean_up_scene()
-    create_background()
+    tree,links = clean_up_scene()
+    # uncooked
+    scene = bpy.context.scene
+    scene.render.use_multiview = True
+    scene.render.views_format = 'STEREO_3D'
+    rl = tree.nodes.new(type="CompositorNodeRLayers")
+    composite = tree.nodes.new(type="CompositorNodeComposite")
+    composite.location = 200,0
+
+    scene = bpy.context.scene
+    
+    create_random_cube([], 2, size=[0.7,1.0])
+    create_random_cube("001",5,size=[ 0.7, 1.0])
+#    create_background()
+
+makeascene()
