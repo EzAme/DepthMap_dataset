081048956cab6a144468b2ef65f804d83d675214
brothaman <rbrothers12@gmail.com>
2017-11-16 20:25:46 -0600

    adding light sources

--- pby_fun.py
+++ pby_fun.py
@@ -163,6 +163,10 @@ def clean_up_scene():
     for texture in bpy.data.textures:
         texture.user_clear();
         bpy.data.textures.remove(texture);
+    
+    for lamp in bpy.data.lamps:
+        lamp.user_clear();
+        bpy.data.lamps.remove(lamp);
 
     # clear default nodes
     bpy.context.scene.use_nodes = True
@@ -183,6 +187,53 @@ def clean_up_scene():
          
     return tree, links
 
+def set_up_lighting(scene):
+    #setup lighting:               
+    # create a new lamp
+    lamp_data = bpy.data.lamps.new(name="Lamp", type='POINT')
+    
+    # Create new object with our lamp datablock
+    lamp_object = bpy.data.objects.new(name="New Lamp", object_data=lamp_data)
+
+    # Link lamp object to the scene so it'll appear in this scene
+    scene.objects.link(lamp_object)
+
+    # Place lamp to a specified location
+    lamp_object.location = (5.0, 5.0, 5.0)
+
+    # And finally select it make active
+    lamp_object.select = True
+    scene.objects.active = lamp_object
+
+    light = bpy.data.objects['Lamp']
+    light.data.use_shadow = False   
+    light.data.energy = 5.0         
+    light.location = (8,8,8)        
+    light.select = False            
+
+     # create a new lamp
+    lamp_data = bpy.data.lamps.new(name="Lamp.001", type='POINT')
+    
+    # Create new object with our lamp datablock
+    lamp_object = bpy.data.objects.new(name="New Lamp", object_data=lamp_data)
+
+    # Link lamp object to the scene so it'll appear in this scene
+    scene.objects.link(lamp_object)
+
+    # Place lamp to a specified location
+    lamp_object.location = (5.0, 5.0, 5.0)
+
+    # And finally select it make active
+    lamp_object.select = True
+    scene.objects.active = lamp_object
+
+    light2 = bpy.data.objects['Lamp.001']
+    light2.data.use_shadow = False   
+    light2.data.energy = 5.0         
+    light2.location = (8,-8,8)        
+    light2.select = False  
+    
+    
 def render_scene( links, scene, rl, composite, ID):
                                                                            
     #output the stereoscopic images:                                   
@@ -211,6 +262,7 @@ def makeascene():
     create_random_cube([], 2, size=[0.7,1.0])
     create_random_cube("001",5,size=[ 0.7, 1.0])
     render_scene( links, scene, rl, composite,1)
+    set_up_lighting(2)
 #    create_background()
 
 makeascene()
