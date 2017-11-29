9676b8061bd1e96530ca4c887e2607293ad706b2
brothaman <rbrothers12@gmail.com>
2017-11-29 17:13:48 -0600

    updated to support no absence of objects

--- domainRandomization/dr_script.py
+++ domainRandomization/dr_script.py
@@ -24,13 +24,13 @@ def makeascene():
 
 if __name__ == "__main__":
     # the number of scenes
-    N = 6
+    N = 2000
 
     for i in range(N):
         # create a scene
         makeascene()
         
-        j = randint(1,5)
+        j = randint(0,5)
         p = randint(1,5)
         # add objects randomly
         if i<N/2:
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         else:
             # add rowdy and add j random shapes with textures
             fun.import_rowdy(filename="rowdy.stl",
-                    R=[0,2],
+                    R=[4,6],
                     range_theta=[-0.7853981634,2.3561944902],
                     range_phi=[0,1.25],
                     size=[0.05,0.03])
@@ -50,7 +50,7 @@ if __name__ == "__main__":
         # now that the scene has been created we need to now render the scene
         for k in range(p):
             fun.create_lamp(R=15,
-                    range_theta=[-0.7853981634,2.3561944902],
+                    range_theta=[0,3.14159265],
                     range_phi=[0,1.5707963268])
             
         for k in range(5):
@@ -58,6 +58,12 @@ if __name__ == "__main__":
                     range_theta=[0,1.5707963268],
                     range_phi=[0,1.5707963268])
             if k == 0:
-                fun.render_scene(id="",ofilename="drimages/set"+str(i)+"_image"+str(k)+".png")
+                if i<N/2:
+                    fun.render_scene(id="",ofilename="drimages/not_rowdy/set"+str(i)+"_image"+str(k)+".png")
+                else:
+                    fun.render_scene(id="",ofilename="drimages/rowdy/set"+str(i)+"_image"+str(k)+".png")
             else:
-                fun.render_scene(id=".00"+str(k),ofilename="drimages/set"+str(i)+"_image"+str(k)+".png")
+                if i<N/2:
+                    fun.render_scene(id=".00"+str(k),ofilename="drimages/not_rowdy/set"+str(i)+"_image"+str(k)+".png")
+                else:
+                    fun.render_scene(id=".00"+str(k),ofilename="drimages/rowdy/set"+str(i)+"_image"+str(k)+".png")
